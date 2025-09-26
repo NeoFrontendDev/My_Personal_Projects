95d7afbf7c0443dbfb7357ea6c67f2dfbfaf6c27
Neo Mosebi <neosammosebi01@gmail.com>
2025-09-26 19:02:50 +0200

    Delete my-afrikaans-bot directory

--- my-afrikaans-bot/src/utils/speak.js
+++
@@ -1,8 +0,0 @@
-//Speech Synthesis
-export function speak(text) {
-    if (!("speechSynthesis" in window)) return;
-    const utter = new SpeechSynthesisUtterance(text);
-    utter.lang = "af-ZA";
-    window.speechSynthesis.canvel();
-    window.speechSynthesis.speak(utter);
-}
