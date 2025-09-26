4b871076228fba8c813a13ad09d61254305bd853
Neo <neosammosebi01@gmail.com>
2025-09-26 22:18:09 +0200

    New project created, index.css updated and tailwind.config updated

---
+++ habit-tracker/tailwind.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+    content: ["./index.html", "./src/**/*.{js,jsx}"],
+    theme: {
+        extend: {
+            colors: {
+                tan: "#D2B48C",
+                papayaWhip: "#FFEFD5",
+                ivory: "#FFFFF0"
+            }
+        }
+    },
+    plugins: []
+};
